a886a12bf9b95c4b91a3aec2bf0f2edfbeeda8a8
Prarthan-B-P <prarthanbp11@gmail.com>
2026-05-20 22:59:29 +0530

    Remove Tailwind CDN from static frontend

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: [
+    './frontend/public/index.html',
+    './frontend/public/*.js',
+    './app.js',
+    './events.js',
+    './utils.js'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
